fde49f3356dde1d31d7ff35a1a72d412875ec12f
zhaoqize <ko2338792975@163.com>
2016-08-06 10:37:12 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,22 @@ var path = require('path');
 
 
 module.exports = {
-    entry: path.resolve(__dirname, 'app/main.js'),
+    entry: path.resolve(__dirname, 'app.js'),
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
     },
+    module: {
+	    loaders: [{
+	      test: /\.jsx?$/, 
+	      loader: 'babel',
+	      query: {
+          	presets: ['react', 'es2015']
+          }
+	    },{
+	      test: /\.css$/, 
+	      loader: 'style!css' 
+	    }
+	    ]
+  	}
 };
